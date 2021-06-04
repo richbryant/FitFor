6150991d25e1428761cdbb165f1a0ea28b84f692
Rich Bryant <rich.bryant@live.co.uk>
2021-06-04 16:06:40 +0100

    Add project files.

---
+++ FitFor/Shared/MainHero.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitFor.Shared
+{
+    public partial class MainHero
+    {
+        private string SelectedSlide = "1";
+    }
+}
